707e91f9de74bb42b381a02602401d30afbdc4aa
Abdulaziz <abdulaziz.abdusattorov000@gmail.com>
2024-07-01 20:26:22 +0500

    add swagger urls

--- food_recipe/urls.py
+++ food_recipe/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for food_recipe project.
+URL configuration for social_media project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,43 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Food Recipe API",
+        default_version='v0',
+        description="Test description",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+api_urls = [
+    path('api/v0/user/', include('apps.user.api.v0.urls')),
+    path('api/v0/recipe/', include('apps.recipe.api.v0.urls')),
+    path('api/v0/notification/', include('apps.notification.api.v0.urls')),
+]
+
+urlpatterns += api_urls
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
