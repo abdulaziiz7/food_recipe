d71394845668fafbd540f4a60547fbac5e3ea9b1
Ibodullo01 <ibodullofayzullayev2001@gmail.com>
2024-07-03 21:27:49 +0500

    recipe api comment, tag, category

---
+++ apps/user/api/v0/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+app_name = 'user_api'
+
+urlpatterns = [
+    path('',  name='index'),
+]
--- food_recipe/urls.py
+++ food_recipe/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,26 +30,28 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=[AllowAny],
+    permission_classes=(permissions.AllowAny,),
 )
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-
-
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls)
 ]
-
 api_urls = [
-    path('api/v0/user/', include('apps.user.api.v0.urls')),
+   # path('api/v0/user/', include('apps.user.api.v0.urls')),
     path('api/v0/recipe/', include('apps.recipe.api.v0.urls')),
-    path('api/v0/notification/', include('apps.notification.api.v0.urls')),
+    #path('api/v0/notification/', include('apps.notification.api.v0.urls')),
+]
+urlpatterns += api_urls
+
+
+swagger_path = [
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
-urlpatterns += api_urls
+urlpatterns += swagger_path
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
